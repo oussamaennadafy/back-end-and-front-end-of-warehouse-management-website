2d8332c20f2cbb48276d6b7a33380dced1cab178
oussama <oussa.ennado@gmail.com>
2022-01-23 19:42:53 +0100

    update

--- script.js
+++
@@ -1,6 +0,0 @@
-const headerEl = document.querySelector(".header");
-const buttonEl = document.querySelector(".btn_icons");
-
-buttonEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav_open");
-});
